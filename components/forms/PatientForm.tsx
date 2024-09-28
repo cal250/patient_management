b2a7de5cbe79d993fab009fd58c0ac0b74995cad
cal250 <jeancalvin027@gmail.com>
2024-09-28 20:05:05 +0200

    nice

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -45,7 +45,7 @@ const PatientForm =()=> {
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <section className= "mb-12 space-y-4">
-            <h1 className="header"> Hi there 👋</h1>
+            <h1 className="header"> Hi there,....👋</h1>
             <p className="test-dark-700 ">Schedule your first appointment </p>
 
         </section>
