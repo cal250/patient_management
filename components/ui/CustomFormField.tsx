bcc2b2d173e69460f71facc8e03455bb06c8c6ac
cal250 <jeancalvin027@gmail.com>
2024-09-28 11:06:40 +0200

    half-solved

--- components/ui/CustomFormField.tsx
+++ components/ui/CustomFormField.tsx
@@ -1,86 +1,75 @@
-"use client"
+"use client";
 
-import { Input } from "@/components/ui/input"
-import{
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-FormControl,
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-FormDescription,
-FormField,
-FormItem,
-FormLabel,
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-FormMessage,
-} from "@/components/ui/form"
-import { FormFieldType } from "../forms/PatientForm"
-import { Label } from "./label"
+import { Input } from "@/components/ui/input";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { FormFieldType } from "../forms/PatientForm";
+import { Label } from "./label"; // Ensure Label is correctly imported
 
-import {Control} from "react-hook-form"
-import Image from "next/image"
- 
-interface CustomeProps{
-    control:control<any>,
-    fieldType : FormFieldType,
-    name:string,
-    label?:string,
-    placeholder?:string,
-    iconSrc?:string,
-    iconAlt:string,
-    disabled?:boolean,
-    dateFormat?:string,
-    showTimeSelect?: boolean,
-    children?: React.ReactNode,
-    renderSkeleton?:( field: any) => React.ReactNode,
+import { Control } from "react-hook-form";
+import Image from "next/image";
+
+interface CustomeProps {
+  control: Control<any>;
+  fieldType: FormFieldType;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const RenderField =( {field,props}:{field:any; props :CustomeProps })=>{
-  const {fieldType,iconSrc,iconAlt,placeholder}=props;
+const RenderField = ({ field, props }: { field: any; props: CustomeProps }) => {
+  const { fieldType, iconSrc, iconAlt, placeholder } = props;
 
- switch(fieldType){
-  case FormFieldType.INPUT:
-    return(
-      <div className="flex rounded-md border border-dark-500 bg-dark-400">
-        {iconSrc &&(
-          <Image 
-          src={iconSrc}
-          height= {24}
-          width ={24}
-          alt={iconAlt||'icon'}
-          className="ml-2"
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || "icon"}
+              className="ml-2"
+            />
+          )}
+          {/* Render Input field here */}
+          <Input placeholder={placeholder} {...field} />
+        </div>
+      );
+  }
+};
 
-
-          />
-
-        )}
-
-
-      </div>
-    )
-
- }
-
-}
- const  CustomFormField = (props:CustomeProps) => {
-  const {control, fieldType, name ,label}=props;
+const CustomFormField = (props: CustomeProps) => {
+  const { control, fieldType, name, label } = props;
   return (
     <FormField
-        control={control}
-        name={name}
-        render={({ field }) => (
-          <FormItem className="flex-1">
-            {fieldType !== FormFieldType.CHECKBOX && Label (
-              <FormLabel >{Label}</FormLabel>
-            )}
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+          <RenderField field={field} props={props} />
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
+    />
+  );
+};
 
-            <RenderField field={field} props = {props} />
-
-            <FormMessage className="shad-error" />
-
-          </FormItem>
-         
-        )}
-      />
-  )
-}
-
-export default CustomFormField
+export default CustomFormField;
