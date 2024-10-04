5cd833cb94f3fa8045032c9941cf7da280c5e9fe
cal250 <jeancalvin027@gmail.com>
2024-10-04 12:04:06 +0200

    loader complete

--- components/ui/SubmitButton.tsx
+++ components/ui/SubmitButton.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
-import Image from 'next/image';
+
 import { Button } from './button';
+
+// Define the Loader component outside of the SubmitButton
+const Loader = () => (
+  <svg
+    className="animate-spin"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="4"
+    />
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0a12 12 0 00-12 12h4zm16 0a8 8 0 01-8 8v4a12 12 0 0012-12h-4z"
+    />
+  </svg>
+);
 
 interface ButtonProps {
   isLoading: boolean;
@@ -17,13 +43,7 @@ const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
     >
       {isLoading ? (
         <div className="flex items-center gap-4">
-          <Image
-            src={"/asset/icons/loader.svg"}
-            alt="loader"
-            width={24}
-            height={24}
-            className="animate-spin"
-          />
+          <Loader />
           Loading...
         </div>
       ) : (
